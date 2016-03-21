0046cd86bb051735aa5fbc952dcff295769c4fc9
Jialin Yu <jyu@clearslide.com>
2016-03-20 19:08:28 -0700

    all elements in place

--- betterlearning/education/urls.py
+++ betterlearning/education/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     url(r'^logout$', Logout.as_view(), name='user_logout'),
     url(r'^courses', Courses.as_view(), name='courses_view'),
     url(r'^register', Register.as_view(), name='register_view'),
+    url(r'^question_process', Register.as_view(), name='question_process'),
+    
     
     
 ]
